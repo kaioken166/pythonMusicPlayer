1ec3125644b0bd5d14e08e56c5342c62c767ae9f
nttrung5122 <nguyntrung291@gmail.com>
2023-03-26 00:25:32 +0700

    modify song controller and add play list controller

--- controller/songController.py
+++ controller/songController.py
@@ -7,9 +7,10 @@ from PIL import ImageTk, Image
 class SongController:
 
     def __init__(self):
-        self.pathCurrentSong = ''
-        self.mixer = pygame.mixer
-        self.mixer.init()
+        self.__path_current_song = ''
+        self.__pause = False
+        self.__mixer = pygame.mixer
+        self.__mixer.init()
 
 
     def play_song(self,path):
@@ -20,7 +21,7 @@ class SongController:
     def play_in_time(self, start,isPercent = True):
         
         if isPercent :
-            time = SongController.get_time() / 100 * start
+            time = self.get_time() / 100 * start
         else:
             time = start
         
@@ -28,15 +29,20 @@ class SongController:
 
 
     def get_time(self,song):
-        song = MP3(self.pathCurrentSong)
+        song = MP3(self.__path_current_song)
         song_length = song.info.length
         return song_length
 
     def pause_music(self):
-        self.mixer.music.stop()
+        self.pause = True
+        self.mixer.music.pause()
+        
+    def unpause_music(self):
+        self.pause = False
+        self.mixer.music.unpause()
 
     def get_img(self):
-        audio_file = eyed3.load(self.pathCurrentSong)
+        audio_file = eyed3.load(self.__path_current_song)
         myit = iter(audio_file.tag.images)
         image = next(myit)
         image_file = open("tmp.jpg", "wb")
@@ -47,6 +53,14 @@ class SongController:
         return img
 
     
+    def check_if_finished(self):
+        return not ( self.pause or pygame.mixer.music.get_busy()) # return True if finish
+            
+    def get_info(self):
+        song = eyed3.load(self.__path_current_song)
+        return {
+            'album': song.tag.album,
+            'title': song.tag.album,
+            'artist': song.tag.artist
+        }
 
-
-        
